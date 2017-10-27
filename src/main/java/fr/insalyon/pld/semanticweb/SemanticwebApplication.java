eb4af9c16b6a0bfbebabde5ab01fc72bc6bb0b86
Loic R <rouquette.loic@gmail.com>
2017-10-27 10:36:36 +0200

    Add spring to project

---
+++ src/main/java/fr/insalyon/pld/semanticweb/SemanticwebApplication.java
@@ -0,0 +1,12 @@
+package fr.insalyon.pld.semanticweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanticwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanticwebApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/insalyon/pld/semanticweb/SemanticwebApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.insalyon.pld.semanticweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SemanticwebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
