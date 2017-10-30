d7654b82e3d9db9a9bdcd8a2d59d5558a43dfe4d
MrJuju97351 <justin.gallysp@gmail.com>
2017-10-30 14:14:54 +0100

    Commencement des entitées

---
+++ src/main/java/fr/insalyon/pld/semanticweb/entities/Film.java
@@ -0,0 +1,12 @@
+package fr.insalyon.pld.semanticweb.entities;
+
+import java.util.Date;
+
+public class Film {
+    private Label[] name;
+    private double budget;
+    private double gross;
+    private Date publicationDate;
+
+
+}
