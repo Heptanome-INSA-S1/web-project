6131c0fee83bb955f8f1945e8987a7f789a7f56a
tgay <Tkd-thomas-971@hotmail.fr>
2017-11-03 11:08:44 +0100

    Entity Film completed

--- src/main/java/fr/insalyon/pld/semanticweb/entities/Film.java
+++ src/main/java/fr/insalyon/pld/semanticweb/entities/Film.java
@@ -1,5 +1,6 @@
 package fr.insalyon.pld.semanticweb.entities;
 
+import java.time.Duration;
 import java.util.Date;
 
 public class Film {
@@ -7,6 +8,82 @@ public class Film {
     private double budget;
     private double gross;
     private Date publicationDate;
+    private Personne[] actors;
+    private Personne[] realisators;
+    private Personne[] productors;
+    private String synopsis;
+    private Duration duration;
+    private String type; //String ou class Type ??
 
+    public Label[] getName() {
+        return name;
+    }
 
+    public void setName(Label[] name) {
+        this.name = name;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public double getGross() {
+        return gross;
+    }
+
+    public void setGross(double gross) {
+        this.gross = gross;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Personne[] getActors() {
+        return actors;
+    }
+
+    public void setActors(Personne[] actors) {
+        this.actors = actors;
+    }
+
+    public Personne[] getRealisators() {
+        return realisators;
+    }
+
+    public void setRealisators(Personne[] realisators) {
+        this.realisators = realisators;
+    }
+
+    public Personne[] getProductors() {
+        return productors;
+    }
+
+    public void setProductors(Personne[] productors) {
+        this.productors = productors;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
 }
