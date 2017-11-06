c19077b7f72d14aea6c590c21c4881f280b7a072
MrJuju97351 <justin.gallysp@gmail.com>
2017-11-06 19:13:58 +0100

    Add of @CrossOrigin

--- src/main/java/fr/insalyon/pld/semanticweb/controller/MovieController.java
+++ src/main/java/fr/insalyon/pld/semanticweb/controller/MovieController.java
@@ -5,14 +5,12 @@ import fr.insalyon.pld.semanticweb.repositories.MovieRepository;
 import fr.insalyon.pld.semanticweb.repositories.SPARQLRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin
 @Controller
 public class MovieController {
 
