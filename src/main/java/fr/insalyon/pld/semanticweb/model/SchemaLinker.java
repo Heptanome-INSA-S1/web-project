10ef158a957baaed5b644464228218a37c9fe388
Thoms971 <32865578+Thoms971@users.noreply.github.com>
2017-11-06 19:28:10 +0100

    Update SchemaLinker.java

--- src/main/java/fr/insalyon/pld/semanticweb/model/SchemaLinker.java
+++ src/main/java/fr/insalyon/pld/semanticweb/model/SchemaLinker.java
@@ -12,8 +12,9 @@ public class SchemaLinker {
         namespace.put("dbo", "http://dbpedia.org/ontology/");
         namespace.put("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
         namespace.put("foaf", "http://xmlns.com/foaf/0.1/");
-        namespace.put("owl", "http://www.w3.org/2002/07/owl#");
         namespace.put("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+        namespace.put("schema", "http://schema.org/");
+        namespace.put("umbel-rc","http://umbel.org/umbel/rc/");
     }
 
     public String namespace(String prefix){
@@ -21,7 +22,7 @@ public class SchemaLinker {
     }
 
     public static class Actor{
-        public List<String> typePredicates = Arrays.asList("dbo:Actor");
+        public List<String> typePredicates = Arrays.asList("dbo:Actor", "umbel-rc:Actor");
         public List<String> namePredicates = Arrays.asList("dbo:birthName","foaf:name","rdfs:label");
         public List<String> playInPredicates = Arrays.asList("dbo:movie");
         public List<String> nationalityPredicates = Arrays.asList("dbo:nationality");
@@ -29,15 +30,17 @@ public class SchemaLinker {
     }
 
     public static class Film{
-        public List<String> typePredicates = Arrays.asList("dbo:Film");
+        public List<String> typePredicates = Arrays.asList("dbo:Film", "schema:Movie");
         public List<String> namePredicates = Arrays.asList("dbo:label", "dbo:originalTitle", "foaf:name");
-        /*public List<String> budgetPredicates = Arrays.asList();
-        public List<String> publicationDate=;-- Predicats non trouves*/
+        public List<String> budgetPredicates = Arrays.asList("dbo:budget");
+        //public List<String> publicationDate=;-- Predicats non trouves
         public List<String> actorsPredicates = Arrays.asList("dbo:starring");
         public List<String> durationPredicates = Arrays.asList("dbo:filmRuntime");
         public List<String> directorsPredicates = Arrays.asList("dbo:director");
-        //public List<String> producersPredicates = Arrays.asList();
+        public List<String> producersPredicates = Arrays.asList("dbo:producer");
+        public List<String> grossPredicates = Arrays.asList("dbo:gross");
         public List<String> synopsisPredicates = Arrays.asList("dbo:abstract", "rdfs:comment");
         public List<String> genrePredicates = Arrays.asList("dbo:genre");
+        public List<String> thumbnailPredicates = Arrays.asList("dbo:thumbnail");
     }
 }
