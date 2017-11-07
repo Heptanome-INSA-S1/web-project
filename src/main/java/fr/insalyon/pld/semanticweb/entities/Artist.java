331905e8f7a8e543f19edcc4d90c3816d840d471
Pierre JAGLIN <pjjaglin@gmail.com>
2017-11-07 11:06:52 +0100

    Modif entity Artist & Add Artist model

--- src/main/java/fr/insalyon/pld/semanticweb/entities/Artist.java
+++ src/main/java/fr/insalyon/pld/semanticweb/entities/Artist.java
@@ -1,18 +1,18 @@
 package fr.insalyon.pld.semanticweb.entities;
 
+import java.net.URI;
 import java.util.List;
 
 public class Artist {
 
     public final Personne information;
-    public final List<Movie> filmography;
-    public final List<Movie> bestMovies;
+    public final List<URI> filmography;
+    public final List<URI> bestMovies;
 
-    public Artist(Personne information, List<Movie> filmography, List<Movie> bestMovies){
+    public Artist(Personne information, List<URI> filmography, List<URI> bestMovies){
         this.information = information;
         this.filmography = filmography;
         this.bestMovies = bestMovies;
     }
-
 
 }
