8bbe3772038c346374424b610a5d95d0cf3eed99
Pierre JAGLIN <pjjaglin@gmail.com>
2017-11-07 11:55:47 +0100

    Add entity Work & Modify Movie/MovieRepository

--- src/main/java/fr/insalyon/pld/semanticweb/repositories/MovieRepository.java
+++ src/main/java/fr/insalyon/pld/semanticweb/repositories/MovieRepository.java
@@ -6,6 +6,8 @@ import fr.insalyon.pld.semanticweb.services.sparqldsl.QueryBuilder;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Component;
 
+import java.net.URI;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -63,14 +65,20 @@ public class MovieRepository implements SPARQLRepository<Movie> {
 
     @Override
     public Movie hydrate(Document document) {
+        String poster = null;
         String frenchName = orNull(() -> document.getElementsByTag("rdfs:label").stream().filter(it -> it.attr("xml:lang").equals("fr")).collect(Collectors.toList()).get(0).text());
         String englishName = orNull(() -> document.getElementsByTag("rdfs:label").stream().filter(it -> it.attr("xml:lang").equals("en")).collect(Collectors.toList()).get(0).text());
+        String title = (frenchName == null ? englishName : frenchName);
+        Date releaseDate = null;
         Double gross = orNull(() -> Double.valueOf(document.getElementsByTag("dbo:gross").get(0).text().replace("E", "E+")));
         Double budget = orNull(() -> Double.valueOf(document.getElementsByTag("dbo:budget").get(0).text().replace("E", "E+")));
         String uri = document.baseUri();
         String enSynopsis = orNull(() -> document.getElementsByTag("dbo:abstract").stream().filter(it -> it.attr("xml:lang").equals("en")).collect(Collectors.toList()).get(0).text());
         String frSynopsis = orNull(() -> document.getElementsByTag("dbo:abstract").stream().filter(it -> it.attr("xml:lang").equals("fr")).collect(Collectors.toList()).get(0).text());
-
-        return new Movie(uri,englishName, frenchName, gross, budget, enSynopsis, frSynopsis);
+        String plot = (frSynopsis == null ? enSynopsis : frSynopsis);
+        Double runtime = orNull(() -> Double.valueOf(document.getElementsByTag("dbo:runtime").get(0).text().replace("E", "E+")));
+        List<String> actors = orNull(() -> document.getElementsByTag("dbo:starring").stream().map(element -> element.attr("rdf:resource")).collect(Collectors.toList()));
+        
+        return new Movie(uri, poster, title, releaseDate, plot, runtime, actors, genres, directors, gross, budget);
     }
 }
