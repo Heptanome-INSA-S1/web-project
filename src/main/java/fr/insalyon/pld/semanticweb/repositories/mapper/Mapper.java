89b4c0765ee5438f0998baf6ddfa31170b6fd981
Justin GALLY <justin.gallysp@gmail.com>
2017-11-10 10:27:45 +0100

    Robert Downey jr. !!!!!!!!

--- src/main/java/fr/insalyon/pld/semanticweb/repositories/mapper/Mapper.java
+++ src/main/java/fr/insalyon/pld/semanticweb/repositories/mapper/Mapper.java
@@ -1,7 +1,9 @@
 package fr.insalyon.pld.semanticweb.repositories.mapper;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public interface Mapper<E, M> {
@@ -22,6 +24,14 @@ public interface Mapper<E, M> {
             .filter(Objects::nonNull)
             .map(this::entityToLightModel)
             .collect(Collectors.toList());
+  }
+
+  default <E> List<E> orEmpty(Supplier<List<E>> supplier) {
+    List<E> result = new ArrayList<>();
+      try {
+        result.addAll(supplier.get());
+      } catch (Exception ignored) {}
+    return result;
   }
 
 }
